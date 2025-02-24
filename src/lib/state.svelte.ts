fe298109a8f2aaec48895bd152f8d830df540142
Bishwas Bhandari <bishwasbh@gmail.com>
2025-02-24 15:11:22 +0545

    chore: override feature added

--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -3,44 +3,47 @@ import { untrack } from 'svelte';
 type StorageType = 'localStorage' | 'sessionStorage';
 
 type TypeRegistry = {
-	[K in PropertyKey]: unknown;
+    [K in PropertyKey]: unknown;
 };
 
 declare global {
-	// @ts-ignore
-	type PodTypeRegistry = TypeRegistry;
+    // @ts-ignore
+    type PodTypeRegistry = TypeRegistry;
 }
 
 type GetTypeFromRegistry<K extends keyof PodTypeRegistry> = PodTypeRegistry[K] extends never
-	? unknown
-	: PodTypeRegistry[K];
+    ? unknown
+    : PodTypeRegistry[K];
 
 function track<K extends keyof PodTypeRegistry, V = GetTypeFromRegistry<K>>(
-	key: K,
-	storage: StorageType,
-	context?: V
+    key: K,
+    storage: StorageType,
+    context?: V,
+    override: boolean = false
 ) {
-	let state = $state<V>(context ?? ({} as V));
+    let state = $state<V>(context ?? ({} as V));
 
-	if (typeof window !== 'undefined') {
-		if (context === undefined) {
-			const storedValue = untrack(() => window[storage].getItem(String(key)));
-			if (storedValue) {
-				try {
-					state = JSON.parse(storedValue) as V;
-				} catch {
-					state = {} as V;
-				}
-			}
-		}
+    if (typeof window !== 'undefined') {
+        const storedValue = untrack(() => window[storage].getItem(String(key)));
+        if (storedValue && !override) {
+            try {
+                state = JSON.parse(storedValue) as V;
+            } catch {
+                state = {} as V;
+            }
+        } else if (override || !storedValue) {
+            state = context ?? ({} as V);
+            const json = JSON.stringify($state.snapshot(state));
+            window[storage].setItem(String(key), json);
+        }
 
-		$effect.pre(() => {
-			const json = JSON.stringify($state.snapshot(state));
-			window[storage].setItem(String(key), json);
-		});
-	}
+        $effect.pre(() => {
+            const json = JSON.stringify($state.snapshot(state));
+            window[storage].setItem(String(key), json);
+        });
+    }
 
-	return state;
+    return state;
 }
 
 /**
@@ -48,11 +51,13 @@ function track<K extends keyof PodTypeRegistry, V = GetTypeFromRegistry<K>>(
  * @param key - The key to store the state.
  * @param storage - The storage type to use.
  * @param context - The initial state or override.
+ * @param override - Whether to override the stored value with the provided context.
  */
 export function pod<K extends keyof PodTypeRegistry>(
-	key: K,
-	storage: StorageType = 'localStorage',
-	context?: GetTypeFromRegistry<K>
+    key: K,
+    storage: StorageType = 'localStorage',
+    context?: GetTypeFromRegistry<K>,
+    override: boolean = false
 ): GetTypeFromRegistry<K> {
-	return track(key, storage, context);
+    return track(key, storage, context, override);
 }
